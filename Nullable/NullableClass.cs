ff153af1b4c8a62069f56b0d361309cea995884e
SamuelJRMarshall <sam.marshall014@gmail.com>
2019-07-05 14:09:20 +0100

    Add nullable code

---
+++ Nullable/NullableClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nullable
+{
+    class NullableClass
+    {
+        public int? Number = null;
+
+        public NullableClass(int? number)
+        {
+            Number = number;
+        }
+    }
+}
--- Nullable/Program.cs
+++ Nullable/Program.cs
@@ -6,7 +6,32 @@ namespace Nullable
     {
         static void Main(string[] args)
         {
+            NullableClass nullableClass = null;
             
+            ///Using the conditional operator
+            //int number = nullableClass.Number.HasValue ? nullableClass.Number.Value : -1;
+
+            ///Null-coalescing operator
+            //int numberAgain = nullableClass.Number ?? -1;
+
+            ///Null-conditional operator
+            int numberAgainAgain = nullableClass?.Number ?? -1;
+            Console.WriteLine(numberAgainAgain);
+
+            ///Null-conditional operator continued
+            NullableClass[] nullableClasses = new NullableClass[]
+            {
+                new NullableClass(1), new NullableClass(null), null
+            };
+            string nC1 = nullableClasses?[0]?.Number.Value.ToString();
+
+            ///Null conditional operator delegate
+            //PropertyChangedEventHandler eventHandler = PropertyChanged;
+            //if (eventHandler != null){
+            //  eventHandler(this, new PropertyChangedEventArgs(...));
+            //}
+
+            //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(...));
         }
     }
 }
